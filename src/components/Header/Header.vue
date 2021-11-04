1dc1feaaa3b1abc9ce60ed8f7a343714469cb353
LinBei905 <82702531+LinBei905@users.noreply.github.com>
2021-11-04 20:49:58 +0800

    add

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Header</h1>
+    <div>
+      <router-link to="/">home</router-link>
+      <router-link to="/search">search</router-link>
+      <router-link to="/register">register</router-link>
+      <router-link to="/login">login</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="less" scoped>
+</style>
